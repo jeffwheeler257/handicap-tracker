e03a84ea27ba0817bbefe178f020dbfe7b92ac50
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2025-07-24 13:32:50 -0600

    Setup history page to test basic connection with backend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './Pages/home'
 import { Login } from './Pages/login'
 import { RoundInput } from './Pages/roundinput'
-import { History } from './Pages/history'
+import History from './Pages/history'
 import { Layout } from './Layout'
 
 function App() {
--- frontend/src/Pages/history.tsx
+++ frontend/src/Pages/history.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 
+const History: React.FC = () => {
+    const [data, setData] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
-export function History() {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get<string>('http://localhost:4000/health');
+                setData(response.data);
+            } catch (err) {
+                if (axios.isAxiosError(err)){
+                    setError(err.message);
+                } else {
+                    setError('Unexpected error')
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+    if (!data) return null;
+    
     return (
         <>
             <h1>Round History</h1>
+            <p>Health check result: {data}</p>
         </>
     )
-}
+};
+
+export default History;
