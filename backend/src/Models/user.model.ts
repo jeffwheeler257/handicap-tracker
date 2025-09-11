65a2cb77d2eada173214557285d075631194f493
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2025-09-11 09:14:52 -0600

    Create user model, one to many relationship with Round.

---
+++ backend/src/Models/user.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document, Types } from 'mongoose';
+import { RoundInterface } from './round.model';
+
+export interface UserInterface extends Document {
+  username: string;
+  password: string;
+  rounds: RoundInterface[];
+}
+
+const UserSchema = new Schema<UserInterface>(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    rounds: [{ type: Schema.Types.ObjectId, ref: 'Round' }],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const User = mongoose.model<UserInterface>('User', UserSchema);
+export default User;
