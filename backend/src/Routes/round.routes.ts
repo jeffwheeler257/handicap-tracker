f5534f7e930c29bd9cac942a73083d1d8a316990
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2025-09-23 13:30:56 -0600

    Update route for getting user rounds.

--- backend/src/Routes/round.routes.ts
+++ backend/src/Routes/round.routes.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "../Middleware/auth.middleware";
 const router = Router();
 
 router.post('/', authMiddleware, RoundController.createRound);
-router.get('/', authMiddleware, RoundController.getUserRounds);
+router.get('/user', authMiddleware, RoundController.getUserRounds);
 router.get('/:id', authMiddleware, RoundController.getRoundById);
 router.put('/:id', authMiddleware, RoundController.updateRound);
 router.delete('/:id', authMiddleware, RoundController.deleteRound);
