fe4802a1fca854c4182ebe1aab1e3b0cb310593e
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2025-09-24 14:33:38 -0600

    Update forms to implement login and registration logic to connect with backend.

--- frontend/src/Components/EntryForm.tsx
+++ frontend/src/Components/EntryForm.tsx
@@ -1,17 +1,19 @@
 export interface EntryFormProps {
-  id: string;
+  type: string;
   label: string;
+  value: string;
 };
 
-export function EntryForm({ id, label }: EntryFormProps) {
+export function EntryForm({ type, label, value }: EntryFormProps) {
   return (
     <div className="flex flex-col sm:flex-row items-center gap-2">
-      <label htmlFor={id} className="w-40 text-sm font-medium">
+      <label htmlFor={type} className="w-40 text-sm font-medium">
         {label}
       </label>
       <input
-        id={id}
+        type={type}
         placeholder={`Enter ${label}`}
+        value={value}
         className="flex-1 rounded-md border border-gray-400 p-2 w-full sm:w-auto"
       />
     </div>
---
+++ frontend/src/Components/RegisterForm.tsx
@@ -0,0 +1,65 @@
+import  { useState, type FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthService from "../Services/AuthService";
+
+export default function RegisterForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      await AuthService.register(username, password);
+      navigate('/home');
+    } catch (error) {
+      if (error instanceof Error){
+        setError(error.message)
+      } else {
+        setError("Unknown error occurred.")
+      }
+    }
+  }
+
+  return (
+    <form 
+      className="flex flex-col gap-4 max-w-xl mx-auto p-4"
+      onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col sm:flex-row items-center gap-2">
+          <label className="w-40 text-sm font-medium">
+            Username
+          </label>
+          <input className="flex-1 rounded-md border border-gray-400 p-2 w-full sm:w-auto"
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row items-center gap-2">
+          <label className="w-40 text-sm font-medium">
+            Password
+          </label>
+          <input className="flex-1 rounded-md border border-gray-400 p-2 w-full sm:w-auto"
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="self-center rounded-md bg-slate-900 text-white hover:bg-slate-800 px-4 py-2"
+        >
+          Register
+        </button>
+        {error && <p className="text-red-500">{error}</p>}
+        <p>Already a user? Login <Link className="font-medium underline" to='/login'>here</Link>!</p>
+    </form>
+  );
+}
