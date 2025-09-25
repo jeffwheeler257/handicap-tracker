8fd8d5c0ac1b8d544de9b16a72d997b00a14fbe3
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2025-09-25 15:10:51 -0600

    Update methods to return data.

--- frontend/src/Services/UserService.ts
+++ frontend/src/Services/UserService.ts
@@ -2,15 +2,22 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+interface UserResponse {
+    id: string,
+    username: string,
+    password: string,
+    rounds: string[],
+}
+
 function authHeader() {
-    const userToken = localStorage.getItem('user');
+    const localUser = localStorage.getItem('user');
     let user = null;
-    if(userToken) {
-        user = JSON.parse(userToken);
+    if(localUser) {
+        user = JSON.parse(localUser);
     }
 
-    if (user && user.accessToken) {
-        return { Authorization: 'Bearer ' + user.accessToken};
+    if (user.token) {
+        return { Authorization:`Bearer ${user.token}`};
     } else {
         return { Authorization: ''};
     }
@@ -18,12 +25,24 @@ function authHeader() {
 
 
 export default class UserService {
-    static async getUser(): Promise<Response> {
-        return axios.get(API_URL + 'users/me', { headers: authHeader() });
+    static async getUser(): Promise<UserResponse> {
+        try {
+            const user = await axios.get(API_URL + 'users/me', { headers: authHeader() });
+            return user.data;
+        } catch (error) {
+            console.error('UserService getUser() Error:', error);
+            throw new Error('Failed to get user data.');
+        }
     }
 
-    static async getHandicap(): Promise<Response> {
-        return axios.get(API_URL + 'users/me/handicap', { headers: authHeader() });
+    static async getHandicap(): Promise<number> {
+        try {
+            const handicap = await axios.get(API_URL + 'users/me/handicap', { headers: authHeader() });
+            return handicap.data;
+        } catch (error) {
+            console.error('UserService getHandicap() Error:', error);
+            throw new Error('Failed to get user handicap.');
+        }
     }
 
 }
